fc8b9ad08f9525c60128a578d01ee114185c7962
razijak <119754102+razijak@users.noreply.github.com>
2022-12-04 02:20:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ from os import getenv
 API_ID = int(getenv("API_ID", "6435225")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5699833340").split()))
+OWNER_ID = int(getenv("OWNER_ID", "5699833340"))
+MONGO_URL = getenv("MONGO_URL, "mongodb+srv://ajii:<razibinraya@cluster0.agh8vha.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN", "5875556518:AAFPFPRlRdE5MKywub1sVvHWRDrU0mTptq4")
 ALIVE_PIC = getenv("ALIVE_PIC")
-ALIVE_TEXT = getenv("ALIVE_TEXT")
-PM_LOGGER = getenv("PM_LOGGER")
+ALIVE_TEXT = getenv("ALIVE_TEXT") #optional
+PM_LOGGER = getenv("PM_LOGGER") 
 LOG_GROUP = getenv("LOG_GROUP")
 
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQBiMZkACR4oEvQaAmoEXLHVNuFb4mpRXiN7SmovmVqkM1ONilvAoMn3VK8J84oqPrHwmZUs2Vzd2cq1_EkAyEEOBpJHtFdshNb9T4DaJhEt9uFz3IA0fQA7kgXeL8nLbl6tV0GBG3zrKi__VADLLHtXWRMo4BANpSiO1hpWbi9rJvM2Wxl9CLdkUzbVIl9x7LMjqOihNGxgybHEK901tyVlgcQGVlFrIPx69ikUpN15fQG5bBEcnHdgEUEieeTLbTumTXqjGDWYfhjvc8u8lzEWWlqMzzu8ksCIJ3vLYS8jo1f_WcNzeH8aoy41CtaCqqC_v5yOAJB2kzsY2ZaMlc6X_bHLcgAAAAFTvI38AA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
